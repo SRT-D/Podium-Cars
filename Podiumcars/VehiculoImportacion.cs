6d2db271f76c87d189ce8603b15d968d2057f09f
SRT-D <danielbenitez200714@gamil.com>
2026-03-12 08:13:11 -0500

    Creacion Nuevas clases, VehIculoImportacion

---
+++ Podiumcars/VehiculoImportacion.cs
@@ -0,0 +1,20 @@
+﻿using Podiumcars;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Podiumcarsñ
+
+    public class VehiculoImportacion : Vehiculo
+{
+    public DateTime FechaLlegada { get; set; }
+
+    public override void MostrarDetalles()
+    {
+        Console.WriteLine($"[Importación] {Marca} {Modelo}");
+        Console.WriteLine($" > Fecha estimada de llegada: {FechaLlegada.ToShortDateString()}");
+    }
+}
+
