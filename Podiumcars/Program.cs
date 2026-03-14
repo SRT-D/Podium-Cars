cd2c1dc7f25fe0bb9065c92ddc62f5d80efc7885
SRT-D <danielbenitez200714@gamil.com>
2026-03-14 13:04:10 -0500

    Se presentaron errores con la actualizacion del codigo final por lo cual se ha tenido que volver a colocar cosas que habian desparecido.

--- Podiumcars/Program.cs
+++ Podiumcars/Program.cs
@@ -4,8 +4,48 @@
     {
         static void Main(string[] args)
         {
-            
+            string menu = "||------PODIUM CARS ------||" + "\n" + "1. Gestionar Usuario" + "\n" + "2. Gestionar Vehiculos de Segunda" + "\n" +
+                  "3. Gestionar Vehiculos importacion" + "\n";
+
+            bool continuar = true;
+
+
+            do
+            {
+                Console.Write(menu);
+                string entrada = Console.ReadLine();
+
+                switch (entrada)
+                {
+                    case "1":
+                        UIUsuarios.GestionarUsuario();
+                        break;
+
+                    case "2":
+                        UIVehiculoSegunda.GestionarVehiculosSegunda();
+                        break;
+
+                    case "3":
+                        UIVehiculoImportacion.GestionarVehiculosImportacion();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Cerrando Podium Cars.");
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion invalida.");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+            while (continuar);
 
         }
     }
+
 }
+    
+
